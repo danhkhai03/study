dca800373e5bfa28c167a52b7a84a32819f8625f
danhkhai03 <danhkhai03@gmail.com>
2026-02-24 16:03:15 +0700

    update: add production CORS origin

--- classpet-api/config/cors.php
+++ classpet-api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:8000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:8000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174','https://classpet-app-xyz789.vercel.app', ],
 
     'allowed_origins_patterns' => [],
 
